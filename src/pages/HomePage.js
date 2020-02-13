f820f7f70f22f80758efdc7bfcf0b4a150e09c22
Billy Saengsouvanna <billy.saengs@gmail.com>
2020-02-13 12:33:33 -0600

    added files for homepage

---
+++ src/pages/HomePage.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import '../styles/HomePage.scss'
+
+export const HomePage = () => (
+    <div className='homepage'>
+        <div className='menu'>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>HATS</h1>
+                </div>
+            </div>
+
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>JACKETS</h1>
+                </div>
+            </div>
+
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>SHOES</h1>
+                </div>
+            </div>
+
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>MEN</h1>
+                </div>
+            </div>
+
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>WOMEN</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+)
