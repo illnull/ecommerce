2eba2482ac85325bcb31a5bdbf3122f98a42d1ca
Billy Saengsouvanna <billy.saengs@gmail.com>
2020-03-17 21:01:50 -0500

    Created custom button and style

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Route, Switch } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import ShopPage from './pages/shop'
 import Header from './components/header/header'
+import SignIn from './pages/sign-in/sign-in'
 import './App.css';
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignIn} />
       </Switch>
     </div>
   );
---
+++ src/pages/sign-in/sign-in.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import SignIn from '../../components/sign-in/sign-in'
+
+import './sign-in.scss'
+
+const SignInSignUp = () => (
+    <div>
+        <div className='signin'>
+            <SignIn />
+        </div>
+    </div>
+)
+
+export default SignInSignUp
